b8f36a955f3d9efeb29d1ccc70fe3a156537075b
Mark Small <m.small@ed.ac.uk>
2015-06-16 14:47:21 +0100

    Initial import of EDINA's Redmine module:
    This is the initial import of our EDINA Redmine module, this
    module will use others to configure our Redmine setup, this
    may or may not be specific to EDINA, only time will tell.

---
+++ manifests/init.pp
@@ -0,0 +1,22 @@
+# == Class: redmine
+#
+# Create installation of redmine if it doesn't already exist.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# === Examples
+#
+#  
+#
+# === Authors
+#
+# Author Name <author@domain.com>
+#
+# === Copyright
+#
+#
+#
+class edina_redmine {
+}
